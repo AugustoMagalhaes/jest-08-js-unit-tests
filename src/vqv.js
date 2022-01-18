91f19e7fbefd170336b06a0af0aabd37aeba2764
AugustoMagalhaes <augustommg@gmail.com>
2022-01-18 20:25:34 -0300

    R03: Criando o corpo da funçao vqv para passar nos testes, dividindo a template literals em 2 partes para nao ser rejeitado pelo Lint.

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  if (!name || !age) {
+    return undefined;
+  }
+  // Os template literals possibilitam quebra de pagina interna aos ``, mas o lint acusava passar da quantidade ideal máxima, dai quebrei em 2 msg.
+  const firstMessage = `Oi, meu nome é ${name}!\n`;
+  const secMessage = `Tenho ${age} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+  const message = firstMessage.concat(secMessage);
+  return message;
+};
 
 module.exports = vqv;
