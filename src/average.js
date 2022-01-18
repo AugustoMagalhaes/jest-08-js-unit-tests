aee06b3655460122cabb2ba62573df1696d3f74e
AugustoMagalhaes <augustommg@gmail.com>
2022-01-18 19:27:25 -0300

    R01: Criando a funçao 'average' que retorna a media arredondada de elementos caso eles existam; se um desses elementos nao for numerico ou caso a array seja vazia, retorna 'undefined'.

--- src/average.js
+++ src/average.js
@@ -12,8 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+  let elementsSum = 0;
+  const amount = array.length;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    elementsSum += array[index];
+  }
+  return Math.round(elementsSum / amount);
 };
 
 module.exports = average;
