b6dfc7257ce10a17b0dd399500eaa422b2901ec3
AugustoMagalhaes <augustommg@gmail.com>
2022-01-21 04:16:56 -0300

    R07: Implementando as funçoes 'calculator' e 'arrayGenerator'.

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -33,8 +33,20 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 1, sub: 0 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 1 ], [ 'sub', 0 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => ({
+    sum: number1 + number2,
+    mult: number1 * number2,
+    div: Math.floor(number1 / number2),
+    sub: number1 - number2,  
+});
 
-const arrayGenerator = (type, object) => {};
+const arrayGenerator = (type, object) => {  
+  const calls = {
+    keys: Object.keys(object), 
+    values: Object.values(object),
+    entries: Object.entries(object),
+  };
+  return calls[type];
+};
 
 module.exports = { calculator, arrayGenerator };
